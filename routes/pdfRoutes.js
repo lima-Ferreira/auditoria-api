6f81cb6c89db1b05c8a825f7178f6efc40e86c89
Lima Ferreira <limaferreira490@gmail.com>
2026-02-09 10:38:21 -0300

    alteração no arquivo pdfRoutes.js

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -22,21 +22,14 @@ router.get("/:id", async (req, res) => {
 
     // Configuração específica para rodar no RENDER
     const browser = await puppeteer.launch({
+      executablePath: "/usr/bin/google-chrome", // Caminho padrão do Google Chrome no Linux do Render
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
         "--disable-dev-shm-usage",
+        "--single-process",
+        "--no-zygote",
       ],
-      // O segredo: Aponta para a pasta .cache dentro do seu projeto no Render
-      executablePath: path.join(
-        process.cwd(),
-        ".cache",
-        "puppeteer",
-        "chrome",
-        "linux-145.0.7632.46",
-        "chrome-linux64",
-        "chrome",
-      ),
     });
 
     const page = await browser.newPage();
